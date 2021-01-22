2be4d645866c046df98d4f9a3d26d05873947d24
Tobias Bieniek <tobias@bieniek.cloud>
2021-01-22 22:49:42 +0100

    Use `anyhow` to simplify error handling

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
+use anyhow::Result;
 use rss::Channel;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> Result<()> {
     let resp = reqwest::get("https://www.segelflug.de/osclass/index.php?page=search&sFeed=rss")
         .await?
         .bytes()
