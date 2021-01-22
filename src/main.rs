f9d39e53a9462580b9c422a86e7e2cec81d6f9b8
Tobias Bieniek <tobias@bieniek.cloud>
2021-01-22 22:42:18 +0100

    Use `tokio` and `reqwest` to download RSS feed file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let resp = reqwest::get("https://www.segelflug.de/osclass/index.php?page=search&sFeed=rss")
+        .await?
+        .text()
+        .await?;
+    println!("{:#?}", resp);
+    Ok(())
 }
