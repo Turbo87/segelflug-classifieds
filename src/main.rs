770d5fa675c47ceb4e35e4de4b1e18221f3a7ce7
Tobias Bieniek <tobias@bieniek.cloud>
2021-01-22 22:49:24 +0100

    Use `rss` to parse feed data

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
+use rss::Channel;
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let resp = reqwest::get("https://www.segelflug.de/osclass/index.php?page=search&sFeed=rss")
         .await?
-        .text()
+        .bytes()
         .await?;
-    println!("{:#?}", resp);
+    let channel = Channel::read_from(&resp[..])?;
+    println!("{:#?}", channel);
     Ok(())
 }
