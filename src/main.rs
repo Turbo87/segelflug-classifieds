8de11a575967d628c2c951d12916673eda98cfca
Tobias Bieniek <tobias@bieniek.cloud>
2021-01-22 23:00:19 +0100

    Extract `FEED_URL` constant

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 use anyhow::Result;
 use rss::Channel;
 
+const FEED_URL: &'static str = "https://www.segelflug.de/osclass/index.php?page=search&sFeed=rss";
+
 #[tokio::main]
 async fn main() -> Result<()> {
-    let resp = reqwest::get("https://www.segelflug.de/osclass/index.php?page=search&sFeed=rss")
-        .await?
-        .bytes()
-        .await?;
+    let resp = reqwest::get(FEED_URL).await?.bytes().await?;
     let channel = Channel::read_from(&resp[..])?;
     println!("{:#?}", channel);
     Ok(())
