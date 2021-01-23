d7ddd0e39e5ec2c8ec4fd336227e2a227dfc3d77
Tobias Bieniek <tobias@bieniek.cloud>
2021-01-23 17:35:05 +0100

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+if [ -z "$TARGET_HOST" ]
+then
+    echo "Please set the TARGET_HOST environment variable";
+    exit 1;
+fi
+
+if [ -z "$TARGET_PATH" ]
+then
+    echo "Please set the $TARGET_PATH environment variable";
+    exit 1;
+fi
+
+readonly TARGET_ARCH=armv7-unknown-linux-musleabihf
+readonly SOURCE_PATH=./target/${TARGET_ARCH}/release/segelflug-classifieds
+
+set -o xtrace
+
+cross build --release --target=${TARGET_ARCH}
+rsync ${SOURCE_PATH} ${TARGET_HOST}:${TARGET_PATH}
+ssh -t ${TARGET_HOST} sudo systemctl restart segelflug-classifieds-bot
