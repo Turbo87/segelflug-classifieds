249655e74b023d839225ad61b5541177d6ab0d24
Tobias Bieniek <tobias@bieniek.cloud>
2021-11-05 18:54:42 +0100

    Extract `ItemWithExtraData` struct

--- src/app.rs
+++ src/app.rs
@@ -1,4 +1,4 @@
-use crate::classifieds::{ClassifiedsApi, ClassifiedsDetails, ClassifiedsItem};
+use crate::classifieds::{ClassifiedsApi, ClassifiedsDetails, ClassifiedsItem, ClassifiedsUser};
 use crate::guids;
 use crate::telegram::TelegramApi;
 use anyhow::Result;
@@ -65,6 +65,54 @@ impl App {
 
     #[instrument(skip(self))]
     async fn handle_item(&self, item: &ClassifiedsItem) -> Result<()> {
+        let item = self.load_extra_data(item).await?;
+
+        // print item to the console
+
+        println!(" - {}", item.title());
+        if let Some(price) = item.price() {
+            println!("   💶  {}", price);
+        }
+        if let (Some(user), Some(emoji)) = (item.user_description(), item.user_emoji()) {
+            println!("   {}  {}", emoji, user);
+        }
+        println!("   {}", item.link());
+        println!();
+
+        // send item to Telegram
+
+        if let Some(telegram) = &self.telegram {
+            let mut text = format!("<a href=\"{}\"><b>{}</b></a>\n", item.link(), item.title());
+            if let Some(price) = item.price() {
+                text += &format!("<b>💶  {}</b>\n", price);
+            }
+            if let (Some(user), Some(emoji)) = (item.user_description(), item.user_emoji()) {
+                let user_link = item.user_link().unwrap();
+                text += &format!("{}  <a href=\"{}\"><b>{}</b></a>\n", emoji, user_link, user);
+            }
+            if let Some(description) = item.description() {
+                text += &format!("\n{}\n", description);
+            }
+            text += &format!("\n{}", item.link());
+
+            telegram.send_message(&text).await?;
+
+            if let Err(error) = self
+                .send_photo_for_item(item.item, item.details.as_ref())
+                .await
+            {
+                event!(Level::WARN, error = ?error, "Failed to send photo to Telegram");
+            }
+        }
+
+        Ok(())
+    }
+
+    #[instrument(skip(self))]
+    async fn load_extra_data<'a>(
+        &self,
+        item: &'a ClassifiedsItem,
+    ) -> Result<ItemWithExtraData<'a>> {
         let link = &item.link;
 
         let details = match self.classifieds.load_details(link).await {
@@ -89,69 +137,11 @@ impl App {
             None => None,
         };
 
-        let title = &item.title;
-        let description = &item.description;
-
-        let price = details.as_ref().and_then(|details| details.price.as_ref());
-        let item_location = details
-            .as_ref()
-            .and_then(|details| details.location.as_ref());
-
-        let user_name = user.as_ref().and_then(|user| user.name.as_ref());
-        let user_location = user.as_ref().and_then(|user| user.location.as_ref());
-
-        let location = item_location.or(user_location);
-
-        let user_description = match (user_name, location) {
-            (Some(name), Some(location)) => Some(format!("{} ({})", name, location)),
-            (Some(name), None) => Some(name.clone()),
-            (None, Some(location)) => Some(location.clone()),
-            (None, None) => None,
-        };
-
-        let user_emoji = match (user_name, location) {
-            (Some(_), Some(_)) => Some("🧑‍✈️"),
-            (Some(_), None) => Some("🧑‍✈️"),
-            (None, Some(_)) => Some("🌍"),
-            (None, None) => None,
-        };
-
-        // print item to the console
-
-        println!(" - {}", title);
-        if let Some(price) = &price {
-            println!("   💶  {}", price);
-        }
-        if let (Some(user), Some(emoji)) = (&user_description, user_emoji) {
-            println!("   {}  {}", emoji, user);
-        }
-        println!("   {}", link);
-        println!();
-
-        // send item to Telegram
-
-        if let Some(telegram) = &self.telegram {
-            let mut text = format!("<a href=\"{}\"><b>{}</b></a>\n", link, title);
-            if let Some(price) = price {
-                text += &format!("<b>💶  {}</b>\n", price);
-            }
-            if let (Some(user), Some(emoji)) = (&user_description, user_emoji) {
-                let user_link = user_link.unwrap();
-                text += &format!("{}  <a href=\"{}\"><b>{}</b></a>\n", emoji, user_link, user);
-            }
-            if let Some(description) = description {
-                text += &format!("\n{}\n", description);
-            }
-            text += &format!("\n{}", link);
-
-            telegram.send_message(&text).await?;
-
-            if let Err(error) = self.send_photo_for_item(item, details.as_ref()).await {
-                event!(Level::WARN, error = ?error, "Failed to send photo to Telegram");
-            }
-        }
-
-        Ok(())
+        Ok(ItemWithExtraData {
+            item,
+            details,
+            user,
+        })
     }
 
     async fn send_photo_for_item(
@@ -187,6 +177,73 @@ impl App {
             println!();
             let secs = mins * 60.;
             sleep(Duration::from_secs_f32(secs)).await;
+        }
+    }
+}
+
+struct ItemWithExtraData<'a> {
+    item: &'a ClassifiedsItem,
+    details: Option<ClassifiedsDetails>,
+    user: Option<ClassifiedsUser>,
+}
+
+impl<'a> ItemWithExtraData<'a> {
+    pub fn title(&self) -> &str {
+        &self.item.title
+    }
+
+    pub fn link(&self) -> &str {
+        &self.item.link
+    }
+
+    pub fn price(&self) -> Option<&str> {
+        self.details
+            .as_ref()
+            .and_then(|details| details.price.as_deref())
+    }
+
+    pub fn description(&self) -> Option<&str> {
+        self.item.description.as_deref()
+    }
+
+    pub fn location(&self) -> Option<&str> {
+        self.item_location().or_else(|| self.user_location())
+    }
+
+    pub fn item_location(&self) -> Option<&str> {
+        self.details
+            .as_ref()
+            .and_then(|details| details.location.as_deref())
+    }
+
+    pub fn user_name(&self) -> Option<&str> {
+        self.user.as_ref().and_then(|user| user.name.as_deref())
+    }
+
+    pub fn user_location(&self) -> Option<&str> {
+        self.user.as_ref().and_then(|user| user.location.as_deref())
+    }
+
+    pub fn user_link(&self) -> Option<&str> {
+        self.details
+            .as_ref()
+            .and_then(|details| details.user_link.as_deref())
+    }
+    pub fn user_description(&self) -> Option<String> {
+        match (self.user_name(), self.location()) {
+            (Some(name), Some(location)) => Some(format!("{} ({})", name, location)),
+            (Some(name), None) => Some(name.to_string()),
+            (None, Some(location)) => Some(location.to_string()),
+            (None, None) => None,
+        }
+    }
+
+    pub fn user_emoji(&self) -> Option<&str> {
+        match (self.user_name(), self.location()) {
+            (Some(_), Some(_)) => Some("🧑‍✈️"),
+            (Some(_), None) => Some("🧑‍✈️"),
+            (None, Some(_)) => Some("🌍"),
+            (None, None) => None,
         }
     }
 }
